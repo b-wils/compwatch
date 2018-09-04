36b874c369c62c032c9b4ef5d96c2fe8bba950fe
Brandon <bmw8719@gmail.com>
2018-09-04 14:30:24 -0500

    Display images for each hero

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import firebase from './firebase.js'; // <--- add this line
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {heroes: []}
+  }
   render() {
     return (
       <div className="App">
@@ -14,18 +18,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. My edits! IS this gonig???
         </p>
+        {this.state.heroes.map((item,i) => <span key={item.name}><img src={getImageFromHero(item)}/></span>)}
       </div>
     );
+
   }
 
   componentDidMount() {
     firebase.firestore().collection('heroes').get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${doc.data()}`);
-            })
+            this.setState({heroes: querySnapshot.docs.map((doc) => doc.data()) })
         });
-
   }
+}
+
+function getImageFromHero(hero) {
+  return "/images/heroes/Icon-" + hero.name.toLowerCase().replace(/[^\w\s]|_/g, "").replace(/\s+/g, "_") + ".png";
 }
 
 export default App;
