fad6db5478d26d83d0fb21c400b67caf6e4cb971
Brandon <bmw8719@gmail.com>
2018-09-04 12:04:55 -0500

    Scripts to connect to firebase and populate heroes

---
+++ scripts/add-heroes.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+var heroes = require('./heroes');
+
+var firebase = require('firebase-admin');
+
+var serviceAccount = process.env.FIREBASE_ADMIN_CONFIG;
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://compwatch-207022.firebaseio.com"
+});
+
+var db = firebase.firestore();
+
+
+heroes.forEach((hero) => {
+	var docRef = db.collection('heroes').doc(hero.name);
+	var setHero = docRef.set({hero});
+})
+
---
+++ scripts/heroes.js
@@ -0,0 +1,114 @@
+module.exports =  heroes = [
+{
+	"name": "Ana",
+	"role": "Support"
+},
+{
+	"name": "Mercy",
+	"role": "Support"
+},
+{
+	"name": "Lucio",
+	"role": "Support"
+},
+{
+	"name": "Zenyatta",
+	"role": "Support"
+},
+{
+	"name": "Moira",
+	"role": "Support"
+},
+{
+	"name": "Brigette",
+	"role": "Support"
+},
+{
+	"name": "D.Va",
+	"role": "Tank"
+},
+{
+	"name": "Orisa",
+	"role": "Tank"
+},
+{
+	"name": "Reinhardt",
+	"role": "Tank"
+},
+{
+	"name": "Roadhog",
+	"role": "Tank"
+},
+{
+	"name": "Winston",
+	"role": "Tank"
+},
+{
+	"name": "Wrecking Ball",
+	"role": "Tank"
+},
+{
+	"name": "Zarya",
+	"role": "Tank"
+},
+{
+	"name": "Bastion",
+	"role": "Damage"
+},
+{
+	"name": "Doomfist",
+	"role": "Damage"
+},
+{
+	"name": "Genji",
+	"role": "Damage"
+},
+{
+	"name": "Hanzo",
+	"role": "Damage"
+},
+{
+	"name": "Junkrat",
+	"role": "Damage"
+},
+{
+	"name": "McCree",
+	"role": "Damage"
+},
+{
+	"name": "Mei",
+	"role": "Damage"
+},
+{
+	"name": "Pharah",
+	"role": "Damage"
+},
+{
+	"name": "Reaper",
+	"role": "Damage"
+},
+{
+	"name": "Solider: 76",
+	"role": "Damage"
+},
+{
+	"name": "Sombra",
+	"role": "Damage"
+},
+{
+	"name": "Symmetra",
+	"role": "Damage"
+},
+{
+	"name": "Torbjorn",
+	"role": "Damage"
+},
+{
+	"name": "Tracer",
+	"role": "Damage"
+},
+{
+	"name": "Widowmaker",
+	"role": "Damage"
+},
+]
