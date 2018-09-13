acdd8d0ba7acb6e3368999e086b1ea4b52d311c2
Brandon <bmw8719@gmail.com>
2018-09-13 14:48:52 -0500

    First pass at flexbox, fix bug where heroes couldnt unselect

--- src/MatchEntry/MatchEntryPresentation.js
+++ src/MatchEntry/MatchEntryPresentation.js
@@ -6,14 +6,14 @@ class MatchEntryPresentation extends Component {
     return (
       <div className="MatchEntry">
         {this.props.message ? <div> {this.props.message}</div> : null}
-        <form onSubmit={this.props.handleSubmit}>
-          <div>
+        <FlexContainer onSubmit={this.props.handleSubmit}>
+          <div style={{width: '100%'}}>
             Current SR: <input type="text" name="currentSR" value={this.props.currentSR} onChange={this.props.currentSRChange}/>
             New SR: <input type="text" name="newSR" value={this.props.newSR} onChange={this.props.newSRChange}/>
             Result: {this.props.result} SR Change: {this.props.SRDiff}
           </div>
 
-          <div>
+          <div style={{'maxWidth': '900px', margin: '10px'}}>
           {this.props.sortedHeroes ? 
             Object.keys(this.props.sortedHeroes).map((heroType, i) => {
               return (
@@ -21,7 +21,7 @@ class MatchEntryPresentation extends Component {
                   <div>{heroType}:</div>
                   
                   {this.props.sortedHeroes[heroType].map((item,i) => 
-                    <PickerElement key={item.name} imgUrl={getImageFromHero(item)} name={item.name} onChange={this.props.heroSelectChange} checked={this.props.selectedHeroes[item.name]  ? true : false}/>
+                    <PickerElement type='checkbox' key={item.name} imgUrl={getImageFromHero(item)} name={item.name} onChange={this.props.heroSelectChange} checked={this.props.selectedHeroes[item.name]  ? true : false}/>
                   )}
                   
                 </div>
@@ -39,7 +39,7 @@ class MatchEntryPresentation extends Component {
                   <div>{mapType}:</div>
                   
                   {this.props.sortedMaps[mapType].map((item,i) => 
-                    <PickerElement imgUrl={getImageFromMap(item)} key={item.name} name={item.name} onChange={this.props.mapSelectChange} checked={this.props.selectedMap === item.name  ? true : false}/>
+                    <PickerElement type='radio' imgUrl={getImageFromMap(item)} key={item.name} name={item.name} onChange={this.props.mapSelectChange} checked={this.props.selectedMap === item.name  ? true : false}/>
                   )}
                   
                 </div>
@@ -50,8 +50,8 @@ class MatchEntryPresentation extends Component {
           </div>
 
 
-           <div><input type="submit" value="Submit" /></div> 
-        </form>
+           <div style={{width: '100%'}}><input type="submit" value="Submit" /></div> 
+        </FlexContainer>
       </div>
     );
 
@@ -66,10 +66,10 @@ function getImageFromMap(map) {
   return process.env.PUBLIC_URL + "/images/maps/" + map.name.toLowerCase().replace(/[^\w\s]|_/g, "").replace(/\s+/g, "_") + "_link.png";
 }
 
-const PickerElement = ({name, onChange, checked, imgUrl}) => {
+const PickerElement = ({name, onChange, checked, imgUrl, type}) => {
   return (
       <span>
-        <input type="radio" name={name} id={name} style={{display:'none'}} onChange={onChange} checked={checked} value={checked}/>
+        <input type={type} name={name} id={name} style={{display:'none'}} onChange={onChange} checked={checked} value={checked}/>
         <PickerItem htmlFor={name} checked={checked}>
           <PickerImage src={imgUrl} alt={name} title={name} />
           <PickerText> {name} </PickerText>
@@ -77,6 +77,13 @@ const PickerElement = ({name, onChange, checked, imgUrl}) => {
       </span>
     );
 };
+
+const FlexContainer = styled.form `
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
 
 const PickerItem = styled.label `
       height: 90px;
