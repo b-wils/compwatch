8430667da149cf0dbbdc88e8f5e7da5a5211d0d4
Brandon <bmw8719@gmail.com>
2019-04-02 21:09:52 -0500

    Fixes breaking changes with react-scripts + react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,25 @@
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+} = require("customize-cra");
+
 const { injectBabelPlugin } = require('react-app-rewired');
 
-module.exports = function override(config, env) {
-	config = injectBabelPlugin(
-		['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-		config,
-	);
-	return config;
-};
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd", libraryDirectory: "es", style: 'css'
+  }),
+  // addLessLoader({
+  //   javascriptEnabled: true,
+  //   modifyVars: { "@primary-color": "#1DA57A" }
+  // })
+);
+
+// module.exports = function override(config, env) {
+// 	config = injectBabelPlugin(
+// 		['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+// 		config,
+// 	);
+// 	return config;
+// };
